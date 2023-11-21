6310fa7b0b9f234ccfccde64d0431c5227f485b1
Luis Hernandez <luis07hernandez05@gmail.com>
2023-11-21 10:31:22 -0600

    feat: add slicky carrousel

--- web/modules/custom/prueba_module/js/carruselTrack.js
+++ web/modules/custom/prueba_module/js/carruselTrack.js
@@ -1,21 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const container = document.querySelector("#carousel-track");
-  const children = document.querySelector("#carrusel-item");
+window.onload = function () {
+  jQuery('#carousel-track').slick({
+    autoplay: false,
+    autoplaySpeed: 1500,
+    arrows: true,
+    prevArrow: '.carrusel-prev',
+    nextArrow: '.carrusel-next',
+    slidesToShow: 4,
+    slidesToScroll: 1
+  });
+};
 
-  document
-    .querySelector(".carrusel-prev")
-    .addEventListener("click", function () {
-      // Calcula el ancho del elemento a desplazar
-      var scrollAmount = children.offsetWidth;
-      // Desplaza el contenedor suavemente hacia la izquierda
-      container.scrollBy({ left: -scrollAmount, behavior: "smooth" });
-    });
-
-  document
-    .querySelector(".carrusel-next")
-    .addEventListener("click", function () {
-      var scrollAmount = children.offsetWidth;
-      // Desplaza el contenedor suavemente hacia la derecha
-      container.scrollBy({ left: scrollAmount, behavior: "smooth" });
-    });
-});
